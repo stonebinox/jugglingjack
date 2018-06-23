5d0479c7b2a8bdf22f7b2f839c6a4ed5e064bece
Anoop S <anoop.santhanam@gmail.com>
2018-06-24 00:23:40 +0530

    Add pricing model

--- src/Jobs.js
+++ src/Jobs.js
@@ -7,7 +7,7 @@ export class Jobs extends Component {
 
     render() {
         return (
-            <div className="layer" active={this.props.active}>
+            <div className="layer white-overlay" active={this.props.active}>
                 <div className="container">
                     <h1 className="text-center light text-shadow">Job Listings</h1>
                 </div>
