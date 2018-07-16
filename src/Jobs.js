08511097102b628194fe3ae1d572abe3614e1095
Anoop S <anoop.santhanam@gmail.com>
2018-07-16 23:22:33 +0530

    Added momentJS

--- src/Jobs.js
+++ src/Jobs.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
+import Moment from 'react-moment';
 
 export class Jobs extends Component {
     constructor(props) {
@@ -42,7 +43,8 @@ export class Jobs extends Component {
                     var applications = that.state.applicationsArray.slice();
                     applications = applications.concat(response);
                     that.setState({
-                        applicationsArray: applications
+                        applicationsArray: applications,
+                        defaultDisplay: "none"
                     });
                     break;
                     case "NO_APPLICATIONS_FOUND":
@@ -57,17 +59,19 @@ export class Jobs extends Component {
         if (this.state.applicationsArray.length > 0) {
             var applications = this.state.applicationsArray;
             console.log(applications);
-            var layout = (<div className="well">
+            var layout = (<div>
                 <h3 className="text-center">Active Applications</h3>
+                <br/>
                 {applications.map((row, index) => {
                     if ((row != null) && (row != undefined)) {
                         var applicationID = row.idapplication_master;
                         var applicationTitle = row.application_title;
                         var applicationDescription = row.application_description;
                         if (applicationDescription === "") {
-                            applicationDescription = 'No description.';
+                            applicationDescription = row.company_master_idcompany_master.company_description;
                         }
                         var companyName = row.company_master_idcompany_master.company_name;
+                        var time = row.timestamp;
                         return (
                             <div className="panel panel-info" key={"jobs_" + index}>
                                 <div className="panel-heading bold">{applicationTitle}</div>
@@ -77,6 +81,7 @@ export class Jobs extends Component {
                                 <small className="bold">{companyName}</small>
                                 </div>
                                 <div className="panel-footer text-right">
+                                    <Moment fromNow>{time}</Moment>&nbsp;&nbsp;
                                     <button type="button" className="btn btn-primary btn-sm">Apply</button>
                                 </div>
                             </div>
@@ -106,7 +111,8 @@ export class Jobs extends Component {
                     <hr />
                     <p>No listings yet! Check back for updates.</p>
                 </div>
-                <div className="container-fluid" style={{display: this.state.jobsDisplay}}>
+                <br/>
+                <div className="container" style={{display: this.state.jobsDisplay}}>
                     {this.displayApplications()}
                 </div>
             </div>
