f637ca84f0c8f2d069cc34aca4668b0fdb7b0e10
Anoop S <anoop.santhanam@gmail.com>
2018-07-18 06:30:35 +0530

    Fixed sign up bug

--- src/MessageBox.js
+++ src/MessageBox.js
@@ -7,6 +7,28 @@ export class MessageBox extends Component {
     }
 
     render() {
-        
+        setTimeout(function(){
+            $("#myModal").modal("show");
+        }, 500);
+        return (
+            <div class="container-fluid">
+                <div id="myModal" className="modal fade" role="dialog">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <a href="#" data-dismiss="modal" className="close">&times;</a>
+                                <h4 className="modal-title">{this.props.data.title}</h4>
+                            </div>
+                            <div className="modal-body">
+                                <p>{this.props.data.body}</p>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
